20e0b594ea301a62c0611056428c6d7e3b893174
Miguel Torres <migueltg93@gmail.com>
2018-05-02 13:39:52 -0500

    basic changes

--- dog_spec.rb
+++ dog_spec.rb
@@ -1,6 +1,23 @@
 require 'rspec'
 require_relative 'dog'
 
-describe Dog do 
+RSpec.describe Dog do 
+  describe '#bark' do 
+    it 'returns "Woof"' do  
+      expect(subject.bark).to eql('Woof!')
+    end
+  end
 
+  describe '#hungry?' do 
+    it 'returns true if hunger_level is more than 5' do 
+      dog = described_class.new(hunger_level: 7) 
+      expect(dog).to be_hungry 
+    end
+
+    it 'returns false if hunger_level is 5 or less' do 
+      dog = described_class.new(hunger_level: 5) 
+      expect(dog).to_not be_hungry 
+    end
+
+  end
 end
